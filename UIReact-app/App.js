0c7e72051eddfd0de9632af508448ac67d6dcc92
zackia <Zainzakia99@gmail.com>
2024-02-06 09:52:53 +0700

    baru

--- UIReact-app/App.js
+++ UIReact-app/App.js
@@ -1,16 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/stack';
-import HomeScreen from './components/HomeScreen';
-import DetailsScreen from './components/DetailsScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen.js';
+import DetailsScreen from './src/screens/DetailsScreen.js';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="Details" component={DetailsScreen}/>
       </Stack.Navigator>
